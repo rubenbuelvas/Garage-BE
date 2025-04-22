2bc8fa8d7aa44950aa677160f28e0160cc38f3b1
rubenbuelvas <rcbuelvasv@gmail.com>
2025-04-22 14:50:39 -0500

    Add categories and status to product model

--- src/api/model/model.go
+++ src/api/model/model.go
@@ -1,9 +1,37 @@
 package model
 
+const (
+	StatusAvailable  = "available"
+	StatusOutOfStock = "out_of_stock"
+
+	CategoryElectronics    = "electronics"
+	CategoryClothing       = "clothing"
+	CategoryHomeAppliances = "home_appliances"
+	CategoryBooks          = "books"
+	CategoryPlants         = "plants"
+	CategoryAutomotive     = "automotive"
+)
+
+var Categories = []string{
+	CategoryElectronics,
+	CategoryClothing,
+	CategoryHomeAppliances,
+	CategoryBooks,
+	CategoryPlants,
+	CategoryAutomotive,
+}
+
+var Statuses = []string{
+	StatusAvailable,
+	StatusOutOfStock,
+}
+
 type Product struct {
 	Name        string   `json:"name"`
 	Description string   `json:"description"`
 	ImageURLs   []string `json:"image_urls"`
+	Category    string   `json:"category"`
+	Status      string   `json:"status"`
 	Price       float64  `json:"price"`
 	Quantity    int      `json:"quantity"`
 	CreatedAt   string   `json:"created_at"`
